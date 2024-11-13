49b2eb6756b34c7a7b9abdc6392719f71560d82b
yavarna venkatesh <149807257+Venkatesh280@users.noreply.github.com>
2024-11-13 21:47:34 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,12 +1,11 @@
 package com.driver;
 
 public class Main {
-   private String name;
-
-    public String getName(){
-        return name;
+  public static void main(String args[]){
+        RWOnly a=new RWOnly();
+        //try, setting a value to name, by directly accessing it using obj and also try printing it
+        //The field RWOnly.name is not visible
+        //a.name="venky";
+        a.setName("venky");
+        System.out.println(a.getName());
     }
-    public void setName(String name){
-        this.name=name;
-    }
-}
